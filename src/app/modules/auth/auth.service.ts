bd99e588caaa3899de4faa13db00cc0daf1d9752
fahimahammed <fahimahammed.cse@gmail.com>
2024-12-26 18:28:37 +0600

    :bulb: auth added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -4,45 +4,63 @@ import User from "../user/user.model";
 import { IAuth, IJwtPayload } from "./auth.interface";
 import { createToken } from "./auth.utils";
 import config from "../../config";
+import mongoose from "mongoose";
 
 const loginUser = async (payload: IAuth) => {
-  const user = await User.isUserExistsByEmail(payload.email);
-  if (!user) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found !');
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const user = await User.findOne({ email: payload.email }).session(session);
+    if (!user) {
+      throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found!');
+    }
+
+    if (!user.isActive) {
+      throw new AppError(StatusCodes.FORBIDDEN, 'This user is not active!');
+    }
+
+    if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+      throw new AppError(StatusCodes.FORBIDDEN, 'Password does not match');
+    }
+
+    const jwtPayload: IJwtPayload = {
+      userId: user._id as string,  // Ensure _id is cast to a string
+      role: user.role,
+    };
+
+    const accessToken = createToken(
+      jwtPayload,
+      config.jwt_access_secret as string,
+      config.jwt_access_expires_in as string,
+    );
+
+    const refreshToken = createToken(
+      jwtPayload,
+      config.jwt_refresh_secret as string,
+      config.jwt_refresh_expires_in as string,
+    );
+
+    const updateUserInfo = await User.findByIdAndUpdate(user._id,
+      { clientInfo: payload.clientInfo, lastLogin: Date.now() },
+      { new: true, session });
+
+    await session.commitTransaction();
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
   }
-
-  if (!user.isActive) {
-    throw new AppError(StatusCodes.FORBIDDEN, 'This user is not active !');
-  }
-
-  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
-    throw new AppError(StatusCodes.FORBIDDEN, 'Password do not matched');
-  }
-
-  const jwtPayload: IJwtPayload = {
-    userId: user._id as string,
-    role: user.role,
-  };
-
-  const accessToken = createToken(
-    jwtPayload,
-    config.jwt_access_secret as string,
-    config.jwt_access_expires_in as string,
-  );
-
-  const refreshToken = createToken(
-    jwtPayload,
-    config.jwt_refresh_secret as string,
-    config.jwt_refresh_expires_in as string,
-  );
-
-  return {
-    accessToken,
-    refreshToken
-  };
-
 };
 
 export const AuthService = {
   loginUser
-}
+};
