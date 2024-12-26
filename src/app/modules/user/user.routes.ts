033c85043315de7499add0478e431abf56fc0353
fahimahammed <fahimahammed.cse@gmail.com>
2024-12-26 16:50:14 +0600

    :bulb: add zod validations

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { UserController } from './user.controller';
 import clientInfoParser from '../../middleware/clientInfoParser';
+import validateRequest from '../../middleware/validateRequest';
+import { UserValidation } from './user.validation';
 
 const router = Router();
 
 // Define routes
-router.post('/', clientInfoParser, UserController.registerUser);
+router.post('/',
+    clientInfoParser,
+    validateRequest(UserValidation.userValidationSchema),
+    UserController.registerUser
+);
 
 export const UserRoutes = router;
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,11 +1,25 @@
 import { z } from 'zod';
+import { UserRole } from './user.interface';
 
-export const userValidation = {
-  create: z.object({
+const clientInfoSchema = z.object({
+  device: z.enum(['pc', 'mobile']).optional().default('pc'), // Allow only 'pc' or 'mobile'
+  browser: z.string().min(1, 'Browser name is required'),
+  ipAddress: z.string().min(1, 'IP address is required'),
+  pcName: z.string().optional(), // Optional field
+  os: z.string().optional(), // Optional field
+  userAgent: z.string().min(1, 'User agent is required'),
+});
+
+const userValidationSchema = z.object({
+  body: z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
     name: z.string().min(1, 'Name is required'),
-  }),
-  update: z.object({
-    id: z.string().uuid('Invalid ID format'),
-    name: z.string().optional(),
-  }),
-};
+    role: z.enum([UserRole.CUSTOMER, UserRole.VENDOR]).default(UserRole.CUSTOMER), // Match enum values in your code
+    clientInfo: clientInfoSchema // Nested schema for client info
+  })
+});
+
+export const UserValidation = {
+  userValidationSchema
+}
