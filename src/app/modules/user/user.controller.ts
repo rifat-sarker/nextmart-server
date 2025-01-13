d1722a6d3e214f843189a39d692f23905f94ee10
fahimahammed <fahimahammed.cse@gmail.com>
2025-01-13 13:11:46 +0600

    updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -8,29 +8,28 @@ import config from '../../config';
 import { IJwtPayload } from '../auth/auth.interface';
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
-   const result = await UserServices.registerUser(req.body);
 
-  const result = await UserServices.registerUser(
-    req.body
-  );
+   const result = await UserServices.registerUser(
+      req.body
+   );
 
-  const { refreshToken, accessToken } = result;
+   const { refreshToken, accessToken } = result;
 
-  res.cookie('refreshToken', refreshToken, {
-    secure: config.NODE_ENV === 'production',
-    httpOnly: true,
-    sameSite: 'none',
-    maxAge: 1000 * 60 * 60 * 24 * 365,
-  });
+   res.cookie('refreshToken', refreshToken, {
+      secure: config.NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+   });
 
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'User logged in successfully!',
-    data: {
-      accessToken,
-    },
-  });
+   sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: 'User logged in successfully!',
+      data: {
+         accessToken,
+      },
+   });
 });
 
 
@@ -76,7 +75,6 @@ const updateUserStatus = catchAsync(async (req, res) => {
 export const UserController = {
    registerUser,
    getAllUser,
-   registerVendor,
    updateUserStatus,
    updateProfile,
 };
