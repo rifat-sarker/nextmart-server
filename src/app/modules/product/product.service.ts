a4956c55b31207d4f948180098b02730b8705f21
mizanmahi <mizanmahi24@gmail.com>
2025-01-15 00:35:43 +0600

    merged with development

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/appError';
 import { IImageFile, IImageFiles } from '../../interface/IImageFile';
@@ -7,25 +6,10 @@ import User from '../user/user.model';
 import { IProduct } from './product.interface';
 import { Category } from '../category/category.model';
 import { Product } from './product.model';
-import { log } from 'console';
 import QueryBuilder from '../../builder/QueryBuilder';
 import { ProductSearchableFields } from './product.constant';
 import { Order } from '../order/order.model';
 import Shop from '../shop/shop.model';
-=======
-import { StatusCodes } from "http-status-codes";
-import AppError from "../../errors/appError";
-import { IImageFile, IImageFiles } from "../../interface/IImageFile";
-import { IJwtPayload } from "../auth/auth.interface";
-import User from "../user/user.model";
-import { IProduct } from "./product.interface";
-import { Category } from "../category/category.model";
-import { Product } from "./product.model";
-import QueryBuilder from "../../builder/QueryBuilder";
-import { ProductSearchableFields } from "./product.constant";
-import { Order } from "../order/order.model";
-import Shop from "../shop/shop.model";
->>>>>>> b15b9fe2cb2e230f3e442ae78314a0443ce9632c
 
 const createProduct = async (
    productData: Partial<IProduct>,
@@ -152,111 +136,41 @@ const createProduct = async (
 // }
 
 const getAllProduct = async (query: Record<string, unknown>) => {
-<<<<<<< HEAD
    const { minPrice, maxPrice, ...pQuery } = query;
+
    const productQuery = new QueryBuilder(
-      Product.find().populate('reviews'),
+      Product.find()
+         .populate('category', 'name')
+         .populate('shop', 'name')
+         .populate('brand', 'name'),
       pQuery
    )
-      .search(ProductSearchableFields)
+      .search(['name', 'description'])
       .filter()
       .sort()
       .paginate()
       .fields()
       .priceRange(Number(minPrice) || 0, Number(maxPrice) || Infinity);
 
-   const result = await productQuery.modelQuery;
+   const products = await productQuery.modelQuery.lean();
+
+   const productsWithOfferPrice = await Promise.all(
+      products.map(async (product) => {
+         const productDoc = await Product.findById(product._id);
+         const offerPrice = await productDoc?.offerPrice;
+         return {
+            ...product,
+            offerPrice: Number(offerPrice) || null,
+         };
+      })
+   );
+
    const meta = await productQuery.countTotal();
 
    return {
       meta,
-      result,
+      result: productsWithOfferPrice,
    };
-=======
-  const { minPrice, maxPrice, ...pQuery } = query;
-
-  const productQuery = new QueryBuilder(Product.find()
-    .populate('category', 'name')
-    .populate('shop', 'name')
-    .populate('brand', 'name'), pQuery)
-    .search(['name', 'description'])
-    .filter()
-    .sort()
-    .paginate()
-    .fields()
-    .priceRange(Number(minPrice) || 0, Number(maxPrice) || Infinity);
-
-  const products = await productQuery.modelQuery.lean();
-
-  const productsWithOfferPrice = await Promise.all(products.map(async (product) => {
-    const productDoc = await Product.findById(product._id);
-    const offerPrice = await productDoc?.offerPrice;
-    return {
-      ...product,
-      offerPrice: Number(offerPrice) || null,
-    };
-  }));
-
-  const meta = await productQuery.countTotal();
-
-  return {
-    meta,
-    result: productsWithOfferPrice,
-  };
-};
-
-
-const getTrendingProducts = async (limit: number) => {
-  const now = new Date();
-  const last30Days = new Date(now.setDate(now.getDate() - 30));
-
-  const trendingProducts = await Order.aggregate([
-    {
-      $match: {
-        createdAt: { $gte: last30Days },
-      },
-    },
-    {
-      $unwind: "$products",
-    },
-    {
-      $group: {
-        _id: "$products.product",
-        orderCount: { $sum: "$products.quantity" },
-      },
-    },
-    {
-      $sort: { orderCount: -1 },
-    },
-    {
-      $limit: limit || 10,
-    },
-    {
-      $lookup: {
-        from: "products",
-        localField: "_id",
-        foreignField: "_id",
-        as: "productDetails",
-      },
-    },
-    {
-      $unwind: "$productDetails",
-    },
-    {
-      $project: {
-        _id: 0,
-        productId: "$_id",
-        orderCount: 1,
-        name: "$productDetails.name",
-        price: "$productDetails.price",
-        offer: "$productDetails.offer",
-        imageUrls: "$productDetails.imageUrls",
-      },
-    },
-  ]);
-
-  return trendingProducts;
->>>>>>> b15b9fe2cb2e230f3e442ae78314a0443ce9632c
 };
 
 const getTrendingProducts = async (limit: number) => {
@@ -310,6 +224,58 @@ const getTrendingProducts = async (limit: number) => {
 
    return trendingProducts;
 };
+
+// const getTrendingProducts = async (limit: number) => {
+//    const now = new Date();
+//    const last30Days = new Date(now.setDate(now.getDate() - 30));
+
+//    const trendingProducts = await Order.aggregate([
+//       {
+//          $match: {
+//             createdAt: { $gte: last30Days },
+//          },
+//       },
+//       {
+//          $unwind: '$products',
+//       },
+//       {
+//          $group: {
+//             _id: '$products.product',
+//             orderCount: { $sum: '$products.quantity' },
+//          },
+//       },
+//       {
+//          $sort: { orderCount: -1 },
+//       },
+//       {
+//          $limit: limit || 10,
+//       },
+//       {
+//          $lookup: {
+//             from: 'products',
+//             localField: '_id',
+//             foreignField: '_id',
+//             as: 'productDetails',
+//          },
+//       },
+//       {
+//          $unwind: '$productDetails',
+//       },
+//       {
+//          $project: {
+//             _id: 0,
+//             productId: '$_id',
+//             orderCount: 1,
+//             name: '$productDetails.name',
+//             price: '$productDetails.price',
+//             offer: '$productDetails.offer',
+//             imageUrls: '$productDetails.imageUrls',
+//          },
+//       },
+//    ]);
+
+//    return trendingProducts;
+// };
 
 export const ProductService = {
    createProduct,
